cf555bbcabee30e24749d0cca1430b9cdb321565
MerojT <merojbektursunboyev@gmail.com>
2026-07-17 16:51:18 +0500

    Fix Fix

--- Frontend/src/pages/Player.jsx
+++ Frontend/src/pages/Player.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import api from "../api/axios";
 import { Navigate } from "react-router-dom";
-const KODIK_BASE_URL = "https://aniqit.com/serial";
+const KODIK_BASE_URL = "https://gocdn.lol/serial";
 
 function Player() {
   const { id } = useParams();
@@ -88,6 +88,7 @@ function Player() {
   const getPlayerSrc = () => {
     if (!anime) return null;
     if (anime.playerUrl) return anime.playerUrl;
+    
     if (anime.malId) {
       return `${KODIK_BASE_URL}/${anime.malId}/iframe`; 
     }
